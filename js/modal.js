a10ce499fc234be966fa04949ee25b1a4aa7d52e
zahargnat <superzaharic@gmail.com>
2023-03-15 20:46:25 +0200

    123

--- js/modal.js
+++ js/modal.js
@@ -10,9 +10,8 @@ refs.closeModalBtn.addEventListener('click', toggleModal);
 refs.backdrop.addEventListener('click', logBackdropClick);
 
 function toggleModal(){
-    refs.backdrop.classList.toggle('is-hidden');
-}
+refs.backdrop.classList.toggle('is-hidden');}
 
 function logBackdropClick() {
-    console.log('Клік у бекдроп');
+console.log('Клік у бекдроп');
 }
